78bd45957e1e20b210153d893299f37558ef5b25
Rangamani75 <rangamaniranganathappagari@gmail.com>
2025-10-06 22:04:00 +0530

    Add files via upload

---
+++ factorial/factorial.js
@@ -0,0 +1,13 @@
+function factorial(n) {
+  if (typeof n !== "number") return "Error: Input must be a number.";
+  if (n < 0) return "Factorial is not defined for negative numbers.";
+  if (n === 0) return 1;
+
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+module.exports = factorial;
---
+++ factorial/index.js
@@ -0,0 +1,7 @@
+const factorial = require("./factorial");
+console.log(`Factorial of 5 is: ${factorial(5)}`);
+console.log(`Factorial of 7 is: ${factorial(7)}`);
+console.log(`Factorial of 10 is: ${factorial(10)}`);
+console.log(factorial(-3));   
+console.log(factorial("abc"));  
+console.log(`Factorial of 0 is: ${factorial(0)}`); 
